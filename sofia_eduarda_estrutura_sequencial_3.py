67a23c828855dc62fd93e4ffc0683b51404173ed
sofiaeduarda <sofiaeduardamc@gmail.com>
2025-10-15 20:18:58 -0300

    alterações

---
+++ sofia_eduarda_estrutura_sequencial_3.py
@@ -0,0 +1,5 @@
+# Faça um programa que peça um número e então mostre a mensagem "O número informado foi [número]":
+# Desenvolvido por Sofia Eduarda
+
+numero = int(input("Insira um número: "))
+print(f"O número informado foi: {numero}")
