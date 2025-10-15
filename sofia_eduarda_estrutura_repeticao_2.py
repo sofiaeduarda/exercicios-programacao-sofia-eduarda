82d39979d14d8fb5462bee0dbdc3f005d72f582e
sofiaeduarda <sofiaeduardamc@gmail.com>
2025-10-15 20:47:55 -0300

    altreações

--- sofia_eduarda_estrutura_repeticao_2.py
+++ sofia_eduarda_estrutura_repeticao_2.py
@@ -1,4 +1,4 @@
-# Faça um programa que leia 5 números e informe o maior número.
+# Faça um programa que leia 5 números e informe a soma e a média dos números.
 # Desenvolvido por Sofia Eduarda
 
 soma = 0
