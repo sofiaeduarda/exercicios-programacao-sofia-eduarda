ac6a512ff46a3a10e846698f4b1b8c894fe71929
sofiaeduarda <sofiaeduardamc@gmail.com>
2025-10-29 16:18:40 -0300

    atividade 1

--- sofia_eduarda_estrutura_decisao_1.py
+++
@@ -1,9 +0,0 @@
-# Faça um programa que peça dois números e imprima o maior deles.
-# Desenvolvido por Sofia Eduarda
-
-numero1 = int(input("Digite o seu primeiro número: "))
-numero2 = int(input("Digite o seu segundo número: "))
-if numero1 > numero2:
-    print(f"{numero1} é o maior.")
-else:
-    print(f"{numero2} é o maior")
